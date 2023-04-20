5f6341e8ffb9d4413222db0493658e8e2aec0910
Sanetro <50074470+sanetro@users.noreply.github.com>
2023-04-20 22:10:34 +0200

    "init"

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
-/*document.querySelector("#plus-button").addEventListener("click", () => {
-  const resultElement = document.querySelector("#result");
-  resultElement.innerText = parseFloat(resultElement.innerText) + 1;
-});*/
-
+// Printing numbers to #number monitor --- START
 function numberClickListener(char) {
   return () => {
     const element = document.querySelector("#number");
@@ -24,6 +20,8 @@ document.querySelector("#seven-button").addEventListener("click", numberClickLis
 document.querySelector("#eight-button").addEventListener("click", numberClickListener(8));
 document.querySelector("#nine-button").addEventListener("click", numberClickListener(9));
 document.querySelector("#zero-button").addEventListener("click", numberClickListener(0));
+// Printing numbers to #number monitor --- END
+
 
 document.querySelector("#dot-button").addEventListener("click", () => {
   const element = document.querySelector("#number");
@@ -34,30 +32,26 @@ document.querySelector("#dot-button").addEventListener("click", () => {
 
 document.querySelector("#remove-last-char-button").addEventListener("click", () => {
   const element = document.querySelector("#number");
-  element.innerText= element.innerText.substring(0, element.innerText.length -1);
+  element.innerText = element.innerText.substring(0, element.innerText.length - 1);
 
-  if(element.innerText.length === 0){
-    element.innerText = "0";
+  if(element.innerText.length === 0) {
+    element.innerText = '0';
   }
 });
 
 document.querySelector("#remove-all-button").addEventListener("click", () => {
-  document.querySelector("#result").innerText = "0";
-  document.querySelector("#number").innerText = "0";
+  document.querySelector("#result").innerText = '0';
+  document.querySelector("#number").innerText = '0';
 });
 
-document.querySelector("#enter-button").addEventListener("click", () => {
-  document.querySelector("#result").innerText = document.querySelector("#number").innerText;
-  document.querySelector("#number").innerText = "0";
-});
+
 
 function operationClickListener(operation) {
   return () => {
-    const result = document.querySelector("#result");
-    const number = document.querySelector("#number");
-
-    result.innerText = Math.round(operation(parseFloat(result.innerText), parseFloat(number.innerText)) * 100000)/100000;
-    number.innerText = "0";
+    const resultElement = document.querySelector("#result");
+    const numberElement = document.querySelector("#number");
+    resultElement.innerText = Math.round(operation(parseFloat(resultElement.innerText), parseFloat(numberElement.innerText)) * 100000) / 100000;
+    numberElement.innerText = '0';
   }
 }
 
@@ -65,3 +59,45 @@ document.querySelector("#plus-button").addEventListener("click", operationClickL
 document.querySelector("#minus-button").addEventListener("click", operationClickListener((a, b) => a - b));
 document.querySelector("#multiply-button").addEventListener("click", operationClickListener((a, b) => a * b));
 document.querySelector("#divide-button").addEventListener("click", operationClickListener((a, b) => a / b));
+document.querySelector("#percent-button").addEventListener("click", operationClickListener((a, b) => a * (b / 100)));
+
+
+let lastClickedOperation;
+function setLastOperation (nameOperation) {
+  lastClickedOperation =  nameOperation;
+}
+document.querySelector("#enter-button").addEventListener("click", () => {
+  const resultElement = document.querySelector("#result");
+  const numberElement = document.querySelector("#number");
+  switch (lastClickedOperation) {
+    case "add":
+      console.log('s');
+      resultElement.innerText = Math.round(parseFloat(document.querySelector("#result").innerText) + parseFloat(document.querySelector("#number").innerText) * 100000) / 100000;
+      break;
+    case "minus":
+      resultElement.innerText = Math.round(parseFloat(document.querySelector("#result").innerText) - parseFloat(document.querySelector("#number").innerText) * 100000) / 100000;
+      break;
+    case "multiply":
+      resultElement.innerText = Math.round(parseFloat(document.querySelector("#result").innerText) * parseFloat(document.querySelector("#number").innerText) * 100000) / 100000;
+      break;
+    case "divide":
+      resultElement.innerText = Math.round(parseFloat(document.querySelector("#result").innerText) / parseFloat(document.querySelector("#number").innerText) * 100000) / 100000;
+      break;
+    case "percent":
+      resultElement.innerText = Math.round(parseFloat(document.querySelector("#result").innerText) * (parseFloat(document.querySelector("#number").innerText) * 100) * 100000) / 100000;
+      break;
+    default:
+      alert("error");
+      break;
+  }
+  numberElement.innerText = '0';
+});
+
+
+document.querySelector("#enter-button").addEventListener("click", setLastOperation("add"));
+document.querySelector("#enter-button").addEventListener("click", setLastOperation("minus"));
+document.querySelector("#enter-button").addEventListener("click", setLastOperation("multiply"));
+document.querySelector("#enter-button").addEventListener("click", setLastOperation("divide"));
+document.querySelector("#enter-button").addEventListener("click", setLastOperation("percent"));
+
+
